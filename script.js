a45cbd9ab91b6b3e31b7cfd0b4f8a564f3656121
Mahadi9990 <mdshafayat9999@gmail.com>
2025-08-29 21:00:27 +0600

    i can reduce call start value

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const callHistory = []
+
+
+
+
 const hearts = document.getElementsByClassName("heart_icon");
 
 for (let i = 0; i < hearts.length; i++) {
@@ -6,6 +11,21 @@ for (let i = 0; i < hearts.length; i++) {
     const totalHeart = heartNumber + 1;
     document.getElementById("heart_number").innerText = totalHeart;
   });
+}
+
+const calls = document.getElementsByClassName("call");
+
+for (let i = 0; i < calls.length; i++) {
+    calls[i].addEventListener("click", () => {
+      const reduceNumber =parseInt(document.getElementById("callCount_Number").innerText)
+      if(reduceNumber <= 0){
+        alert("Your are not able to call Star count value is less than 0")
+        return
+      }
+      const totalreduceNumber = reduceNumber - 20
+      document.getElementById("callCount_Number").innerText = totalreduceNumber;
+      alert('HELLOW')
+    });
 }
 
 
