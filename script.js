a61d7a232d4500c471632bdfdb3ab24bf31a0701
Mahadi9990 <mdshafayat9999@gmail.com>
2025-08-29 21:21:11 +0600

    clear History done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const callHistory = []
+// history
+
+let callHistory = []
 
 
-
+// add hearts
 
 const hearts = document.getElementsByClassName("heart_icon");
 
@@ -13,21 +15,36 @@ for (let i = 0; i < hearts.length; i++) {
   });
 }
 
+// call section
+
 const calls = document.getElementsByClassName("call");
 
 for (let i = 0; i < calls.length; i++) {
     calls[i].addEventListener("click", () => {
       const reduceNumber =parseInt(document.getElementById("callCount_Number").innerText)
       if(reduceNumber <= 0){
-        alert("Your are not able to call Star count value is less than 0")
+        alert("Your are not able to make a call Star count value is less than 0")
         return
       }
       const totalreduceNumber = reduceNumber - 20
       document.getElementById("callCount_Number").innerText = totalreduceNumber;
       alert('HELLOW')
+      const data ={
+        title:calls[i],
+        date: new Date().toLocaleTimeString()
+      }
+      callHistory.push(data)
+      console.log(callHistory)
     });
 }
 
 
+
+// clear History
+
+document.getElementById("clear_btn").addEventListener('click',()=>{
+    callHistory = null
+    console.log(callHistory)
+})
 
 
