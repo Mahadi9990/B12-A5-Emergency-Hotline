ccd9274e2a5af38f1d2866e7df7089df02b0de3b
Mahadi9990 <mdshafayat9999@gmail.com>
2025-09-11 14:34:39 +0600

    finish

--- script.js
+++ script.js
@@ -1,14 +1,77 @@
 const load = () => {
   const emergencyHelplines = [
-    { id: 1, name: "National Emergency Number", subName: "National Emergency", number: "999", category: "All", imageData: "https://placehold.co/64x64/png?text=999" },
-    { id: 2, name: "Police Helpline Number", subName: "Police", number: "999", category: "Police", imageData: "https://placehold.co/64x64/png?text=Police" },
-    { id: 3, name: "Fire Service Number", subName: "Fire", number: "999", category: "Fire", imageData: "https://placehold.co/64x64/png?text=Fire" },
-    { id: 4, name: "Ambulance Service", subName: "Ambulance", number: "1994-999999", category: "Ambulance", imageData: "https://placehold.co/64x64/png?text=Ambulance" },
-    { id: 5, name: "Women & Child Helpline", subName: "Women & Child Helpline", number: "109", category: "Women & Child", imageData: "https://placehold.co/64x64/png?text=Women" },
-    { id: 6, name: "Anti-Corruption Helpline", subName: "Anti-Corruption", number: "106", category: "Govt.", imageData: "https://placehold.co/64x64/png?text=Govt" },
-    { id: 7, name: "Electricity Helpline", subName: "Electricity Outage", number: "16216", category: "Electricity", imageData: "https://placehold.co/64x64/png?text=Electric" },
-    { id: 8, name: "Brac Helpline", subName: "Brac", number: "16445", category: "NGO", imageData: "https://placehold.co/64x64/png?text=Brac" },
-    { id: 9, name: "Bangladesh Railway Helpline", subName: "Travel", number: "163", category: "Travel", imageData: "https://placehold.co/64x64/png?text=Travel" }
+    {
+      id: 1,
+      name: "National Emergency Number",
+      subName: "National Emergency",
+      number: "999",
+      category: "All",
+      imageData: "assets/emergency.png",
+    },
+    {
+      id: 2,
+      name: "Police Helpline Number",
+      subName: "Police",
+      number: "999",
+      category: "Police",
+      imageData: "assets/police.png",
+    },
+    {
+      id: 3,
+      name: "Fire Service Number",
+      subName: "Fire",
+      number: "999",
+      category: "Fire",
+      imageData: "assets/fire-service.png",
+    },
+    {
+      id: 4,
+      name: "Ambulance Service",
+      subName: "Ambulance",
+      number: "1994-999999",
+      category: "Ambulance",
+      imageData: "assets/ambulance.png",
+    },
+    {
+      id: 5,
+      name: "Women & Child Helpline",
+      subName: "Women & Child Helpline",
+      number: "109",
+      category: "Women & Child",
+      imageData: "assets/coin.png",
+    },
+    {
+      id: 6,
+      name: "Anti-Corruption Helpline",
+      subName: "Anti-Corruption",
+      number: "106",
+      category: "Govt.",
+      imageData: "assets/heart.png",
+    },
+    {
+      id: 7,
+      name: "Electricity Helpline",
+      subName: "Electricity Outage",
+      number: "16216",
+      category: "Electricity",
+      imageData: "assets/police.png",
+    },
+    {
+      id: 8,
+      name: "Brac Helpline",
+      subName: "Brac",
+      number: "16445",
+      category: "NGO",
+      imageData: "assets/brac.png",
+    },
+    {
+      id: 9,
+      name: "Bangladesh Railway Helpline",
+      subName: "Travel",
+      number: "163",
+      category: "Travel",
+      imageData: "assets/ambulance.png",
+    },
   ];
 
   const card = document.getElementById("card_area");
@@ -16,19 +79,19 @@ const load = () => {
   for (const item of emergencyHelplines) {
     const createCard = document.createElement("div");
     createCard.innerHTML = `
-      <div class="bg-[#dedce7] rounded-lg p-5 h-68 flex flex-col justify-center item-start">
+      <div class="bg-[#dedce7] rounded-lg p-9 h-90 flex flex-col justify-center item-start gap-3 ">
         <div class="flex justify-between items-center">
-          <div id="image" class="w-[20px] h-[20px]">
-            <img src="assets/emergency.png" alt="" />
+          <div id="image" class="w-[40px] h-[40px]">
+            <img src="${item.imageData}" alt="" />
           </div>
-          <div class="cursor-pointer">
-            <button class="heart_icon">
-              <i class="fa-regular fa-heart"></i>
+          <div class="">
+            <button class="heart_icon cursor-pointer">
+              <i class="fa-regular fa-heart text-4xl"></i>
             </button>
           </div>
         </div>
-        <div>
-          <h1 id="card_title_${item.id}" class="text-2xl font-semibold">${item.name}</h1>
+        <div class="flex flex-col justify-center items-start gap-3">
+          <h1 id="card_title_${item.id}" class=" text-xl md:text-xl lg:text-2xl font-semibold">${item.name}</h1>
           <p class="text-xl font-semibold text-[#9e9297]">${item.subName}</p>
           <h2 id="call_number_${item.id}" class="text-2xl font-semibold">${item.number}</h2>
           <p class="text-xl font-semibold text-[#9e9297]">${item.category}</p>
@@ -44,7 +107,9 @@ const load = () => {
       </div>`;
     card.append(createCard);
 
-    document.getElementById(`call_${item.id}`).addEventListener("click", () => callfunction(item.id));
+    document
+      .getElementById(`call_${item.id}`)
+      .addEventListener("click", () => callfunction(item.id));
   }
 
   const heart_icons = document.querySelectorAll(".heart_icon");
@@ -59,15 +124,16 @@ const load = () => {
 };
 load();
 
-const copyText = (text)=> {
-  navigator.clipboard.writeText(text)
-          .then(() => {
-            alert('Text copied: ' + text);
-          })
-          .catch(err => {
-            console.error('Failed to copy text: ', err);
-          });
-}
+const copyText = (text) => {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert("Text copied: " + text);
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
+};
 
 let callCount = 100;
 let history = [];
